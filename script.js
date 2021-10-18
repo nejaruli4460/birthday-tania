9651ff131adc22bce4c4c8da61eb5f8aa371a871
nejaruli4460 <79055019+nejaruli4460@users.noreply.github.com>
2021-10-18 14:11:29 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var img=document.querySelector("img");
+var img=document.querySelector(".img");
+var audio=new Audio("Traditional-Happy-Birthday-To-You-Song.mp3");
 img.addEventListener('click',()=>{
-    img.style.display="none";
+    // img.style.display="none";
+    img.src="Birthday_cake.jpg";
     document.querySelector('.click').style.display="none";
+        audio.play();
 })
 let count=0;
 document.querySelector("input").addEventListener('click',()=>{
@@ -9,11 +12,13 @@ document.querySelector("input").addEventListener('click',()=>{
     if(document.querySelector("#opt").value=='Brother'&&count==0){
         document.querySelector(".container").style.display="block";
         document.querySelector(".click").style.display="block";
+        img.style.display="block";
     
     
     }
     else{
-        alert("you dont know about me");
         count++;
+        alert("you dont know about me and you are trying time :"+count);
+        audio.pause();
     }
 });
